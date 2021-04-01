a16c38428e0d7e92094fa14f0b61452c4f1254b4
hungnguyen <hungthezorba@gmail.com>
2021-04-01 21:50:20 +0700

    Add attraction card

---
+++ components/AttractionCard.tsx
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    ImageBackground,
+    FlatList,
+    Dimensions,
+    TouchableOpacity,
+    Image
+} from "react-native";
+import { Ionicons, FontAwesome5, AntDesign } from '@expo/vector-icons';
+
+type CardProps = {
+
+}
+
+
+export default class AttractionCard extends Component<CardProps> {
+    constructor(props: CardProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <TouchableOpacity
+                activeOpacity={0.8} style={style.cardContainer}
+            >
+                <View>
+                    <Image style={style.cardImage} source={{ uri: 'https://dulichkhampha24.com/wp-content/uploads/2020/01/nha-tho-duc-ba-sai-gon-1.jpg' }} />
+                </View>
+                <View style={style.cardBody}>
+                    <View>
+                        <Text style={style.attractionName}>
+                            Nhà Thờ Đức Bà
+                        </Text>
+                    </View>
+                    <View style={style.firstInfo}>
+                        <View style={[style.reward]}>
+                            <FontAwesome5 style={{marginRight: 2}} name="coins" size={24} color="#E2D0A2" />
+                            <Text style={{marginLeft: 2}} >400</Text>
+                        </View>
+                        <View style={{marginRight: 12}}>
+                            <Text style={{color: '#A0A0A0'}}>0.5km</Text>
+                        </View>
+                    </View>
+                    <View style={style.reward}>
+                        <AntDesign style={{marginRight: 2}} name="star" size={24} color="#FF5353" />
+                        <Text style={{marginLeft: 2}}>3.5/5(92)</Text>
+                    </View>
+                </View>
+
+
+            </TouchableOpacity>
+        );
+    }
+}
+
+const style = StyleSheet.create({
+
+    cardContainer: {
+        backgroundColor: '#fff',
+        width: 180,
+        borderRadius: 10,
+    },
+    cardImage: {
+        height: 180,
+        width: '100%',
+        borderRadius: 10,
+    },
+    cardBody: {
+        paddingTop: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 13,
+    },
+    firstInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+
+    },
+    attractionName: {
+        fontSize: 16,
+    },
+    reward: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+
+})
