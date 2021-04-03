9226c287d86a6eff3823c9a5144dc9dc7f1f9d64
CallMeDKay <s3812649@rmit.edu.vn>
2021-04-03 10:15:10 +0700

    Add Animated Input + Screen for login

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import LoginScreen from "./screens/LoginScreen";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StatusBar hidden />
+			<LoginScreen />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#4B8FD2",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, TextInput } from "react-native";
+import FormInput from "../components/FormInput";
+
+const LoginScreen: React.FC = () => {
+	// States defined
+	const [textValue, setTextValue] = useState<string>("");
+	const [passwordValue, setPasswordValue] = useState<string>("");
+
+	// Styles defined
+	const styles = StyleSheet.create({
+		container: {
+			paddingTop: 0,
+			width: "100%",
+		},
+		input: {
+			height: 26,
+			fontSize: 20,
+			color: "#000",
+			borderBottomWidth: 1,
+			borderBottomColor: "#555",
+		},
+	});
+
+	// Methods defined
+	const handleTextChange = (newText: string) => {
+		setTextValue(newText);
+	};
+
+	const handlePasswordChange = (newPassword: string) => {
+		setPasswordValue(newPassword);
+	};
+
+	return (
+		<View style={styles.container}>
+			<FormInput label="Username" value={textValue} onChangeHandler={handleTextChange} />
+			<FormInput
+				isSecured={true}
+				label="Password"
+				value={passwordValue}
+				onChangeHandler={handlePasswordChange}
+			/>
+		</View>
+	);
+};
+
+export default LoginScreen;
