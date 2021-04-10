666106167e7c39d40061585185c27ed3629c1c07
unknown <datngonguyentien@gmail.com>
2021-04-10 11:08:01 +0700

    Rewrite App.tsx

--- App.tsx
+++ App.tsx
@@ -4,27 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import DescriptionTab from './components/DescriptionTab';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AnimatedHeader from './components/AnimatedHeader';
+import MainNav from './navigation/mainNav';
 
 export default function App() {
   return (
-
-    <SafeAreaProvider>
-    <SafeAreaView style={{ flex: 1 }} >
-      <DescriptionTab/>
-      {/* <AnimatedHeader/> */}
-    </SafeAreaView>
-  </SafeAreaProvider>
-  // <View>
-  //   <DescriptionTab/>
-  // </View>
+  <View style={styles.containerLogin}>
+    <StatusBar/>
+    <MainNav/>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerLogin: {
+    flex: 1,
+    backgroundColor: "#4B8FD2"
+  }
 });
