bdf4cd79b2eb592d854503cc5aac28970b69a2ee
hungnguyen <hungthezorba@gmail.com>
2021-04-14 17:51:17 +0700

    Add Journey History screen

--- navigator/ProfileNavigator.tsx
+++ navigator/ProfileNavigator.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from '../screens/ProfileScreen';
 import JourneyHistoryScreen from '../screens/JourneyHistoryScreen'
 import {RootStackParamList} from '../screens/ProfileStackParams'
 
 const ProfileStack = createStackNavigator<RootStackParamList>();
+interface Props {
+    navigation: any
+}
 
 const ProfileNavigator = () => {
     return (
         <ProfileStack.Navigator
+        initialRouteName="Profile screen"
         headerMode="none"
-        initialRouteName="Profile screen">
+        >
             <ProfileStack.Screen
             name="Profile screen"
             component={ProfileScreen}
@@ -23,5 +27,14 @@ const ProfileNavigator = () => {
         </ProfileStack.Navigator>
     )
 }
+
+const style = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        backgroundColor: '#4B8FD2',
+        height: 100,
+        alignItems: 'center'
+    },
+})
 
 export default ProfileNavigator
--- screens/JourneyHistoryScreen.tsx
+++ screens/JourneyHistoryScreen.tsx
@@ -1,13 +1,61 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { FontAwesome5, AntDesign } from '@expo/vector-icons';
 
 
-const JourneyHistoryScreen = () => {
+const JourneyHistoryHeader: React.FC<any> = ({navigation}) => {
+    return (
+        <View style={style.header}>
+        <View>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Text> BACK </Text>
+            </TouchableOpacity>
+        </View>
+        <View style={{flex: 1, marginTop:'7%', justifyContent:'center' ,alignItems: 'center'}}>
+            <Text style={{justifyContent: 'center', fontSize: 18, color: '#fff'}}>Journey History</Text>
+        </View>
+        </View>
+    )
+}
+
+const JourneyHistoryScreen:React.FC<any> = (props) => {
     return (
         <View>
-            <Text>User History</Text>
+            <JourneyHistoryHeader navigation={props.navigation}/>
+            <View>
+                <Text>Completed journey</Text>
+            </View>
+            <View>
+                {/* Card Component */}
+                <View>
+                    <View>
+                        <Text>12 March 2021</Text>
+                    </View>
+                    <View>
+                        <Text>Nhà Thờ Đức Bà</Text>
+                    </View>
+                    <View>
+                        <Text>0.7km</Text>
+                        <Text>-</Text>
+                        <Text>69 Đồng Khởi</Text>
+                    </View>
+                    <View>
+                        <FontAwesome5 style={{marginRight: 2}} name="coins" size={24} color="#E2D0A2" />
+                        <Text style={{marginLeft: 2, fontSize: 18}} >400</Text>
+                    </View>
+                </View>
+            </View>
         </View>        
     )
 }
+
+const style = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        backgroundColor: '#4B8FD2',
+        height: 100,
+        alignItems: 'center'
+    },
+})
 
 export default JourneyHistoryScreen
