d7067c5ee48c77ad68d6c67a8f8946fbd04cf069
hungnguyen <hungthezorba@gmail.com>
2021-05-05 16:13:10 +0700

    Add send expo token

--- navigation/mainNav.tsx
+++ navigation/mainNav.tsx
@@ -1,8 +1,10 @@
 import React, { FC, useState, useEffect } from "react";
 import { getFocusedRouteNameFromRoute, NavigationContainer } from "@react-navigation/native";
 import AuthStack from "./authstack";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, Platform } from "react-native";
 import { storeData, getData } from "../constants/utility";
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
 
 import firebase from "firebase";
 
@@ -87,6 +89,48 @@ const MainNav: FC = () => {
 			}
 		})();
 	}, [userData]);
+
+    async function registerForPushNotificationsAsync() {
+        let token;
+        if (Constants.isDevice) {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+          if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+          }
+          if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+          }
+            token = (await Notifications.getExpoPushTokenAsync()).data;
+            let updates = { 'expoToken': ''}
+            updates.expoToken = token;
+            let userid = await getData("id")
+            console.log(userid)
+            await firebase.database().ref('/users/'+ userid).update(updates)
+
+        } else {
+          alert('Must use physical device for Push Notifications');
+        }
+      
+        if (Platform.OS === 'android') {
+          Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+          });
+        }
+      
+        return token;
+    }
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => {
+
+        })
+    }, [])
+
     return (
         <NavigationContainer>
             {user != null ?
