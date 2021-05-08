341dd02e332b7023ad9c068f20ceafeb423ae5e4
hungnguyen <hungthezorba@gmail.com>
2021-05-08 11:25:21 +0700

    Add call API

--- screens/RankingScreen.tsx
+++ screens/RankingScreen.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import firebase from 'firebase';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View, Image, StyleSheet, useWindowDimensions, Platform } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
@@ -8,19 +8,8 @@ import { getData } from '../constants/utility';
 
 const RankingHeader = () => {
 
-    useEffect(() => {
-        const userID = firebase.auth().currentUser?.uid
-        const url = `https://asia-east2-laca-59b8c.cloudfunctions.net/api/users/${userID}/friendships/leaderboard`
-        axios.get(url)
-        .then(res => {
-            console.log(res)
-        })
-        console.log(url)
-        
-    }, [])
-
-return (
-    <View>
+    return (
+        <View>
             <View style={{ flexDirection: 'row' }}>
                 <View style={{ width: '20%' }}>
                     <Text>Rank</Text>
@@ -33,69 +22,115 @@ return (
                 </View>
             </View>
         </View>
-)}
+    )
+}
 
-const cityRanking = () => {
+const FriendRanking = () => {
+
+    const [leaderboard, setLeaderboard] = useState([])
+
+    useEffect(() => {
+        const userID = firebase.auth().currentUser?.uid
+        const url = `http://localhost:5000/laca-59b8c/asia-east2/api/users/${userID}/friendships/leaderboard`
+        axios.get(url)
+            .then(res => {
+                setLeaderboard(res.data.leaderboard);
+            })
+        console.log(url)
+
+    }, [])
+
+
     return (
         <View>
-            <RankingHeader/>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View style={{ width: '20%' }}>
-                    <Text>1</Text>
-                </View>
+            <RankingHeader />
+            {leaderboard ?
+                <View>
+                    {leaderboard.map((user, index) =>
+                        <View key={user.id} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <View style={{ width: '20%' }}>
+                                <Text>{index + 1}</Text>
+                            </View>
 
-                <View style={{ flexDirection: 'row', alignItems: 'center', width: '60%' }}>
-                    <View>
-                        <Image
-                            source={{ uri: "https://www.pinclipart.com/picdir/middle/577-5772540_ears-clipart-monkey-monkey-avatar-png-transparent-png.png" }}
-                            style={styles.logo}
-                        />
-                    </View>
-                    <View>
-                        <Text>Hung Nguyen</Text>
-                    </View>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', width: '60%' }}>
+                                <View>
+                                    <Image
+                                        source={{ uri: user.urlAvatar }}
+                                        style={styles.logo}
+                                    />
+                                </View>
+                                <View>
+                                    <Text>{user.name}</Text>
+                                </View>
+                            </View>
+                            <View style={{ width: '20%' }}>
+                                <Text>{user.journeyCount}</Text>
+                            </View>
+                        </View>
+                    )}
                 </View>
-                <View style={{ width: '20%' }}>
-                    <Text>10</Text>
-                </View>
-            </View>
+                :
+                null
+            }
+
         </View>
     )
 }
 
 
-const nationalRanking = () => {
+const GlobalRanking = () => {
+
+    const [leaderboard, setLeaderboard] = useState([])
+
+    useEffect(() => {
+        const url = `http://localhost:5000/laca-59b8c/asia-east2/api/users/details/leaderboard`
+        axios.get(url)
+            .then(res => {
+                setLeaderboard(res.data.leaderboard);
+            })
+        console.log(url)
+
+    }, [])
+
+
     return (
         <View>
-            <RankingHeader/>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View style={{ width: '20%' }}>
-                    <Text>2</Text>
-                </View>
-                <View style={{ flexDirection: 'row', alignItems: 'center', width: '60%' }}>
-                    <View>
-                        <Image
-                            source={{ uri: "https://www.pinclipart.com/picdir/middle/577-5772540_ears-clipart-monkey-monkey-avatar-png-transparent-png.png" }}
-                            style={styles.logo}
-                        />
+            <RankingHeader />
+            {leaderboard.map((user) => {
+                return (
+                    <View key={user.id} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={{ width: '20%' }}>
+                            <Text>{user.rank}</Text>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', alignItems: 'center', width: '60%' }}>
+                            <View>
+                                <Image
+                                    source={{ uri: user.urlAvatar }}
+                                    style={styles.logo}
+                                />
+                            </View>
+                            <View>
+                                <Text>{user.name}</Text>
+                            </View>
+                        </View>
+                        <View style={{ width: '20%' }}>
+                            <Text>{user.journeyCount}</Text>
+                        </View>
                     </View>
-                    <View>
-                        <Text>Hung Nguyen</Text>
-                    </View>
-                </View>
-                <View style={{ width: '20%' }}>
-                    <Text>10</Text>
-                </View>
-            </View>
+                )
+            }
+
+            )}
         </View>
     )
 }
 
 
 const renderScene = SceneMap({
-    City: cityRanking,
-    National: nationalRanking,
-  });
+    Friend: FriendRanking,
+    Global: GlobalRanking,
+});
 
 
 const renderTabBar = (props: any) => {
@@ -105,34 +140,34 @@ const renderTabBar = (props: any) => {
         <TabBar
             {...props}
             indicatorStyle={{ backgroundColor: '#8DBAE2', width: indicatorWidth, left: (useWindowDimensions().width / 2 - indicatorWidth) / 2 }}
-            style={{elevation:0, backgroundColor: '#fff', marginTop: Platform.OS? 50:20}}
+            style={{ elevation: 0, backgroundColor: '#fff', marginTop: Platform.OS ? 50 : 20 }}
             activeColor={'#8DBAE2'}
             inactiveColor={'#8DBAE2'}
         />
     )
 
 }
-  
+
 
 const RankingScreen = () => {
     const layout = useWindowDimensions();
 
     const [index, setIndex] = React.useState(0);
     const [routes] = React.useState([
-      { key: 'City', title: 'City' },
-      { key: 'National', title: 'National' },
+        { key: 'Friend', title: 'Friend' },
+        { key: 'Global', title: 'Global' },
     ]);
 
     return (
-            <TabView
+        <TabView
             navigationState={{ index, routes }}
             renderScene={renderScene}
             onIndexChange={setIndex}
             initialLayout={{ width: layout.width }}
             renderTabBar={renderTabBar}
-            style={{backgroundColor: '#fff'}}
-            sceneContainerStyle={{paddingTop: 30, paddingHorizontal: 20}}
-            />
+            style={{ backgroundColor: '#fff' }}
+            sceneContainerStyle={{ paddingTop: 30, paddingHorizontal: 20 }}
+        />
 
     )
 }
@@ -142,7 +177,7 @@ const styles = StyleSheet.create({
         width: 30,
         height: 30,
         borderRadius: 50
-      },
+    },
 })
 
 export default RankingScreen
